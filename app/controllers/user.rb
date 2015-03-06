a93618cac9154530d925e85cf0b42d96423825fb
Kara A Carrell <karaAJC@gmail.com>
2015-03-06 15:50:53 -0600

    updating user controller for signup route

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,15 +1,15 @@
-get '/users' do
-  @users = User.all
-  erb: 'users/index'
-end
-
 get '/users/new' do #signup
-
+  erb :'user/new'
 end
 
 post '/users' do
-  @user = User.create()
+  @user = User.create(params[:user])
   if @user.valid?
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors
+    erb :'user/new'
   end
 end
 
