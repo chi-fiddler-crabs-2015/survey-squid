cbbddeb98162788717f83797aabbe19094f9c3d8
Ricardo Fleury <rfleury2@gmail.com>
2015-03-07 19:37:41 -0600

    Take created

--- app/controllers/take.rb
+++ app/controllers/take.rb
@@ -1,17 +1,20 @@
+get '/takes' do
+  @surveys = Survey.all
+  erb :'takes/show'
+end
+
 get '/surveys/:id/takes/new' do
-  puts params
   @survey = find_survey(params[:id])
   erb :'takes/new'
 end
 
 post '/surveys/:id/takes' do
   puts params
-  @survey = Survey.find(params[:id])
-  @take = Take.create(taker: current_user, survey: @survey)
-  # @responses = @take.tally_responses(params[:response_ids])
-  # if @take.valid?
-  #   redirect '/surveys'
-  # else
-  #   erb :'takes/new'
-  # end
+  take = current_user.takes.create(survey_id: params[:id])
+  puts take
+  params[:response_ids].each do |response_id|
+    puts response_id
+    take.responses.create(choice_id: response_id)
+  end
+  redirect '/takes'
 end
